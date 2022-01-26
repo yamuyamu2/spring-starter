cd620817c2f2bea04424e0c3bbdde24d3c6e4b07
양승환 <yang.sh@hancom.com>
2022-01-26 23:08:53 +0900

    first commit

---
+++ src/main/java/com/dev/yang/hellodevyang/HelloDevyangApplication.java
@@ -0,0 +1,13 @@
+package com.dev.yang.hellodevyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDevyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDevyangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/yang/hellodevyang/HelloDevyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.yang.hellodevyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDevyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
